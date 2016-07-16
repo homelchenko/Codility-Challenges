5599dcbe240940181c11ebd50c398dad39bb3f35
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-07-16 15:47:22 +0300

    Removed inlined method ConvertToBinary; Removed corresponding tests

--- Codility.Lessons/BinaryGap.cs
+++ Codility.Lessons/BinaryGap.cs
@@ -38,23 +38,5 @@ namespace Codility.Lessons
 
             return maxGapLength;
         }
-
-        public string ConvertToBinary(int N)
-        {
-            int @base = 2;
-            int remainder = N;
-
-            string binary = string.Empty;
-            while (remainder > 0)
-            {
-                int modulus = remainder % @base;
-
-                binary = modulus + binary;
-
-                remainder /= @base;
-            }
-
-            return binary;
-        }
     }
 }
