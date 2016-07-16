ae5b0ce81333419d477cc0c192c49e45e19d6de0
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-07-16 14:30:52 +0300

    Added 2 tests: for Zero input and One input

---
+++ Codility.Lessons.Test/BinaryGapTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+
+using NUnit.Framework;
+
+namespace Codility.Lessons.Test
+{
+    [TestFixture]
+    public class BinaryGapTests
+    {
+        [Test]
+        public void Solution_WhenNIsZero_ShouldReturnZero()
+        {
+            // Arrange
+
+            // Act & Asseert
+            AssertThatThereIsNoGap(0);
+        }
+
+        [Test]
+        public void Solution_WhenNIs1_ShouldReturnZero()
+        {
+            // Arrange
+
+            // Act & Assert
+            AssertThatThereIsNoGap(1);
+        }
+
+        private static void AssertThatThereIsNoGap(int input)
+        {
+            // Arrange
+            var binaryGap = new BinaryGap();
+
+            // Act
+            int maxGapLength = binaryGap.solution(input);
+
+            // Assert
+            maxGapLength.Should().Be(0);
+        }
+    }
+}
