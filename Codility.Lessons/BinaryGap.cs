37ee565e9908c64c259aef423f9ba74567d36b33
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-07-16 15:02:01 +0300

    Implemented the logic that counts '0' s

--- Codility.Lessons/BinaryGap.cs
+++ Codility.Lessons/BinaryGap.cs
@@ -4,7 +4,19 @@
     {
         public int solution(int N)
         {
-            return 0;
+            if (N == 0)
+                return 0;
+
+            string binaryInput = ConvertToBinary(N);
+
+            int gapLength = 0;
+            for (int i = 1; i < binaryInput.Length; i++) // 1st element is to be '1' anyways
+            {
+                if (binaryInput[i] == '0')
+                    gapLength++;
+            }
+
+            return gapLength;
         }
 
         public string ConvertToBinary(int N)
