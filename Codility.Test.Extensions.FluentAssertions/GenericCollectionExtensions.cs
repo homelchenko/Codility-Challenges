5a88ac16066f968908983b8cfa1add91ea03978c
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-07-23 16:34:38 +0300

    Added a new project to hold all the utility methods / extensions to FluentAssertions that might be used in unit tests; Added class to contain extensions to Generic collections

---
+++ Codility.Test.Extensions.FluentAssertions/GenericCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using FluentAssertions;
+using FluentAssertions.Collections;
+
+namespace Codility.Test.Extensions.FluentAssertions
+{
+    public static class GenericCollectionExtensions
+    {
+        public static AndConstraint<GenericCollectionAssertions<T>> Be<T>(this GenericCollectionAssertions<T> assertions, IEnumerable<T> expected, string because = "", params object[] becauseArgs)
+        {
+            T[] bufferedExpected = expected.ToArray();
+
+            return assertions
+                .HaveCount(bufferedExpected.Length, because, becauseArgs)
+                .And.ContainInOrder(bufferedExpected, because, becauseArgs);
+        }
+    }
+}
