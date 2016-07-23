c9cdbe4c5970272708fa2b23ec62629cfa2704f1
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-07-23 14:15:25 +0300

    Added a test to validate that empty array as an input leads to another empty array being returned

--- Codility.Lessons.Test/CyclicRotationTests.cs
+++ Codility.Lessons.Test/CyclicRotationTests.cs
@@ -1,9 +1,26 @@
 ﻿using NUnit.Framework;
 
+using FluentAssertions;
+
 namespace Codility.Lessons.Test
 {
     [TestFixture]
     public class CyclicRotationTests
     {
+        [Test]
+        public void Solution_WhenEmptyArray_ShouldReturnEmptyArray()
+        {
+            // Arrange
+            var cyclicRotation = new CyclicRotation();
+
+            int[] emptyArray = new int[0];
+            int anyShiftSize = 1;
+
+            // Act
+            int[] rotatedArray = cyclicRotation.Solution(emptyArray, anyShiftSize);
+
+            // Assert
+            rotatedArray.Should().BeEmpty();
+        }
     }
 }
