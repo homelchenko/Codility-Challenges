22f8dea7603c37b77187bd37e2056101681f4940
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-07-23 14:04:16 +0300

    Added an empty class that will contain Cyclic Rotation implementation

---
+++ Codility.Lessons/CyclicRotation.cs
@@ -0,0 +1,10 @@
+﻿namespace Codility.Lessons
+{
+    public class CyclicRotation
+    {
+        public int[] solution(int[] A, int K)
+        {
+            return new int[0];
+        }
+    }
+}
