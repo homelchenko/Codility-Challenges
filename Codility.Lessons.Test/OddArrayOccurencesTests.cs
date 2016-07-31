0ad38e4e4424dbdcdb99c867852946ee7cfa171d
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-07-31 12:42:36 +0300

    Factors out a test that validates that 'FindUnpaired' method throws ArgumentException when is passed with an input array

--- Codility.Lessons.Test/OddArrayOccurencesTests.cs
+++ Codility.Lessons.Test/OddArrayOccurencesTests.cs
@@ -12,23 +12,26 @@ namespace Codility.Lessons.Test
         public void FindUnpaired_WhenArrayIsEmpty_ShouldThrow()
         {
             // Arrange
-            OddArrayOccurences arrayOccurences = new OddArrayOccurences();
-
             int[] emptyArray = new int[0];
 
-            Action findUnpairedAction = () => arrayOccurences.FindUnpaired(emptyArray);
-
             // Act & Assert
-            findUnpairedAction.ShouldThrow<ArgumentException>();
+            AssertThrowExceptionWithInputArray(emptyArray);
         }
 
         [Test]
         public void FindUnpaired_WhenArrayLengthIsEven_ShouldThrow()
         {
             // Arrange
-            OddArrayOccurences arrayOccurences = CreateOddArrayOccurences();
-
             int[] arrayOfEvenLength = new int[4];
+
+            // Act & Assert
+            AssertThrowExceptionWithInputArray(arrayOfEvenLength);
+        }
+
+        private static void AssertThrowExceptionWithInputArray(int[] arrayOfEvenLength)
+        {
+            // Arrange
+            OddArrayOccurences arrayOccurences = CreateOddArrayOccurences();
 
             Action findUnpairedAction = () => arrayOccurences.FindUnpaired(arrayOfEvenLength);
 
