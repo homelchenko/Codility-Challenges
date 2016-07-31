9f8e988b1974e5c089c23271049744da43a8d545
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-07-31 12:32:55 +0300

    Adds the test fixture, 1st test to validate the input array is not empty; Adds enough implementation to make test gree

---
+++ Codility.Lessons.Test/OddArrayOccurencesTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+using FluentAssertions;
+
+namespace Codility.Lessons.Test
+{
+    [TestFixture]
+    public class OddArrayOccurencesTests
+    {
+        [Test]
+        public void Solution_WhenArrayIsEmpty_ShouldThrow()
+        {
+            // Arrange
+            int[] emptyArray = new int[0];
+
+            Action findUnpairedAction = () => new OddArrayOccurences().FindUnpaired(emptyArray);
+
+            // Act & Assert
+            findUnpairedAction.ShouldThrow<ArgumentException>();
+        }
+    }
+}
---
+++ Codility.Lessons/OddArrayOccurences.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Codility.Lessons
+{
+    public class OddArrayOccurences
+    {
+        public int solution(int[] A)
+        {
+            return FindUnpaired(A);
+        }
+
+        public int FindUnpaired(int[] array)
+        {
+            if (array.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            return Int32.MinValue;
+        }
+    }
+}
