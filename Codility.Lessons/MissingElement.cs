1ebb3d8a8dec4503d4012db7d6306dc85846ac16
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-08-13 15:20:51 +0300

    Raise an error if the array is empty

---
+++ Codility.Lessons/MissingElement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Codility.Lessons
+{
+    public class MissingElement
+    {
+        public int FindMissingElement(int[] array)
+        {
+            if (array.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            return Int32.MinValue;
+        }
+    }
+}
