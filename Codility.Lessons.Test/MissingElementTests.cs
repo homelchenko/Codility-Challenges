e5b0ecbb6b60f99ce068d0adb245f47f3255e03f
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-08-13 15:19:45 +0300

    Raise an error if the array is empty

---
+++ Codility.Lessons.Test/MissingElementTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+using FluentAssertions;
+
+namespace Codility.Lessons.Test
+{
+    [TestFixture]
+    public class MissingElementTests
+    {
+        [Test]
+        public void FindMissingElement_WhenArrayIsEmpty_ShouldThrow()
+        {
+            // Arrange
+            int[] emptyArray = new int[0];
+
+            Action findMissingElement = () => new MissingElement().FindMissingElement(emptyArray);
+
+            // Act & Assert
+            findMissingElement.ShouldThrow<ArgumentException>();
+        }
+    }
+}
