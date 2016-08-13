6345b12bd8873ab282066af58df0176bda618a1c
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-08-13 13:56:52 +0300

    Raises error if origin is less than destination

---
+++ Codility.Lessons.Test/FrogJumpTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Codility.Lessons.Test
+{
+    [TestFixture]
+    public class FrogJumpTests
+    {
+        private const int AnyJumpLength = 1;
+
+        [Test]
+        public void CalculateJumps_WhenOriginLessThanDestination_ShouldThrow()
+        {
+            // Arrange
+            int origin = 2;
+            int destination = 3;
+
+            Action calculateJumps = () => new JumpCalculator().CalculateJumps(origin, destination, AnyJumpLength);
+
+            // Act & Assert
+            calculateJumps.ShouldThrow<ArgumentException>();
+        }
+    }
+}
---
+++ Codility.Lessons/JumpCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Codility.Lessons
+{
+    public class JumpCalculator
+    {
+        public int CalculateJumps(int origin, int destination, int jumpLength)
+        {
+            if (origin < destination)
+            {
+                throw new ArgumentException();
+            }
+
+            return Int32.MinValue;
+        }
+    }
+}
