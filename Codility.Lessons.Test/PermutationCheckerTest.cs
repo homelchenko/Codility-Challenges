ec9faf230ea43adb73cc5bb5b51ffd4ae6c8919f
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-08-20 15:28:37 +0300

    Throw error if array is empty

---
+++ Codility.Lessons.Test/PermutationCheckerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+using FluentAssertions;
+
+namespace Codility.Lessons.Test
+{
+    [TestFixture]
+    public class PermutationCheckerTest
+    {
+        [Test]
+        public void IsPermutation_WhenArrayIsEmpty_ShouldThrow()
+        {
+            // Arrange
+            int[] emptyArray = new int[0];
+
+            Action isPermutation = () => new PermutationChecker().IsPermutation(emptyArray);
+
+            // Act & Assert
+            isPermutation.ShouldThrow<ArgumentException>();
+        }
+    }
+}
---
+++ Codility.Lessons/PermutationChecker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Codility.Lessons
+{
+    public class PermutationChecker
+    {
+        public bool IsPermutation(int[] array)
+        {
+            throw new ArgumentException();
+        }
+    }
+}
