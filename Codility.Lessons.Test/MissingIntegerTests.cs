90341b654dc71fb4ce65e9f90b6403dc24de6d07
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-08-27 14:46:50 +0300

    Throw exception in 'FindMissingNumber' if provided with empty array

---
+++ Codility.Lessons.Test/MissingIntegerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using FluentAssertions;
+
+namespace Codility.Lessons.Test
+{
+    [TestFixture]
+    public class MissingIntegerTests
+    {
+        [Test]
+        public void FindMissingNumber_WhenArrayIsEmpty_ShouldThrow()
+        {
+            // Arrange
+            int[] emptyArray = new int[0];
+
+            Action findMissingNumber = () => new MissingInteger().FindMissingNumber(emptyArray);
+
+            // Act & Assert
+            findMissingNumber.ShouldThrow<ArgumentException>();
+        }
+    }
+}
---
+++ Codility.Lessons/MissingInteger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Codility.Lessons
+{
+    public class MissingInteger
+    {
+        public void FindMissingNumber(int[] array)
+        {
+            throw new ArgumentException();
+        }
+    }
+}
